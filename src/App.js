73ccd0342645a8a49efa0bbab124ce80a0c43856
polir09 <dondapatipolireddy@gmail.com>
2026-01-03 08:13:24 +0530

    fronted updated categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
+import {Category,Account,Cart} from './Hello.js'
+import {Login} from './Login.jsx'
+import {Register} from './Register.jsx'
+const Header=()=>{
+    const [search,changeSearch]=useState("")    
+    return(
+        <div className="grid grid-flow-col gap-4 bg-gray-300 p-4">
+             <input type="text" placeholder="     searchitem    " name="search" onChange={(e)=>changeSearch(e.target.value)}value={search} className="border-2 border-black bg-white rounded-full"></input>
+            <div className="">
+                <Link to="/Register">Register</Link>
+                <Link to="/Login">Login</Link>
+            </div>
+        </div>
+       
+    )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
+const Footer=()=>{
+    return(
+        <div className="grid grid-flow-col gap-4 bg-gray-300 p-4">
+        <Link to="/Category">Category</Link>
+        <Link to="/Account">Account</Link>
+        <Link to="/Cart">Cart</Link>
+        </div>
+    )
 
+}
+const Body=()=>{
+    return(
+        <div height="50cm" width="100%" className="flex-1 bg-gray-100 overflow-y-auto">
+            <Routes>
+                <Route path="/Category" element={<Category></Category>}></Route>
+                <Route path="/Account" element={<Account></Account>}></Route>
+                <Route path="/Cart" element={<Cart></Cart>}></Route>
+                
+                <Route path="/Register" element={<Register></Register>}></Route>
+            </Routes>
+        </div>
+    )
+}
+const App = () => {
+  return (<>
+    <BrowserRouter>
+      {/* FULL SCREEN LAYOUT */}
+      <div className="h-screen flex flex-col">
+        
+        {/* HEADER (fixed height) */}
+        <Header />
+
+        {/* BODY (FIXED SPACE ALWAYS) */}
+        <main className="flex-1 bg-gray-100 overflow-y-auto">
+          <Routes>
+            <Route path="/" element={<Body />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Category" element={<Category />} />
+            <Route path="/Account" element={<Account />} />
+            <Route path="/Login" element={<Login></Login>}></Route>
+            <Route path="/Cart" element={<Cart />} />
+          </Routes>
+        </main>
+
+        {/* FOOTER (fixed height) */}
+        <Footer />
+      </div>
+    </BrowserRouter>
+  
+        </>
+    )
+}
 export default App;
+// const App=()=>{
+//     return(
+//         <div className="grid grid-flow-col grid-rows-4 gap-4 ">
+//             <div className="border-2 border-red-500 bg-red-500">01</div>
+//             <div className="border-2 border-red-500 bg-red-500">02</div>
+//             <div className="border-2 border-red-500 bg-red-500">03</div>
+//             <div className="border-2 border-red-500 bg-red-500">04</div>
+//             <div className="border-2 border-red-500 bg-red-500">05</div>
+//             <div className="row-span-3">
+//                 <div className="row-start-2">06</div>
+//             </div>
+//             {/* <div class="row-span-3 grid grid-rows-subgrid gap-4">
+//                 <div class="row-start-2">06</div>
+//             </div> */}
+//             <div className="border-2 border-red-500 bg-red-500">07</div>
+//             <div className="border-2 border-red-500 bg-red-500">08</div>
+//             <div className="border-2 border-red-500 bg-red-500">09</div>
+//         </div>
+//     )
+// }
+
---
+++ src/Login.jsx
@@ -0,0 +1,65 @@
+import {useState} from "react"
+import axios from 'axios';
+export const Login=()=>{
+    const [Name,newName]=useState("")
+    const [Email,newEmail]=useState("")
+    const [Password,newPass]=useState("")
+    const [Role,newRole]=useState("")
+    let data;
+ const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post("http://localhost:5000/api/Login", {
+            userName: Name,
+            email: Email,
+            password: Password,
+            role: Role
+        });
+        data=response.data
+        console.log("success", response.data);
+        return(
+            <h1>success,</h1>
+        )
+
+    } catch (error) {
+        console.log("error", error.response?.data || error.message);
+    }
+};
+
+    return(
+        <>
+            <form method="POST" onSubmit={handleSubmit}>
+                <table cellpadding="20" align="center" className="border-2 border-red-900  rounded-full p-4">
+                    <tr>
+                        <td colSpan="2" align="center" className=""><h1>LOGIN FORM</h1></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="userName">UserName: </label></td>
+                        <td><input type="text" className="border-2 border-blue-400 rounded-full" name="userName" value={Name} onChange={(e)=>newName(e.target.value)} placeholder=" Enter your username"></input></td>
+                    </tr>
+                
+                    <tr>
+                        <td><label htmlFor="email">Email: </label></td>
+                        <td><input type="email" className="border-2 border-blue-400 rounded-full" name="email" value={Email} onChange={(e)=>newEmail(e.target.value)} placeholder=" Enter your email"></input></td>
+                    </tr>
+                    
+                    <tr>
+                        <td><label htmlFor="password">Password: </label></td>
+                        <td><input type="password" className="border-2 border-blue-400 rounded-full" name="password" value={Password} onChange={(e)=>newPass(e.target.value)} placeholder=" Enter your password"></input></td>
+                    </tr>
+              
+                    <tr>
+                        <td><label htmlFor="role">Role: </label></td>
+                        <td><input type="text" className="border-2 border-blue-400 rounded-full" name="role" value={Role} onChange={(e)=>newRole(e.target.value)} placeholder=" Enter your Role"></input></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2" align="center"><button type="submit" className=" bg-blue-600 p-2 rounded-full hover:bg-violet-700 active:bg-violet-500 focus:outline-2 focus:outline-offset-2 hover:outline-violet-500">submit</button></td>
+                    </tr>
+                    <tr>{data}</tr>
+                </table>
+                
+            </form>
+
+        </>
+    )
+}
